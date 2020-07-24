f630677f1ba6094a6cfbf948f9b778fd21db5f47
jesse7zhangjc <jesse7zhangjc@gmail.com>
2020-07-23 19:54:59 -0700

    Updates

---
+++ precache-manifest.dcb03cde51b29140cd8a4c3c18bd6037.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8acb5c37bed5ad09a588244937c787d",
+    "url": "/about-jingcheng/index.html"
+  },
+  {
+    "revision": "1bbcd8e23da4b06c8599",
+    "url": "/about-jingcheng/static/css/2.74cf14bc.chunk.css"
+  },
+  {
+    "revision": "664e8e5aba02faea5da2",
+    "url": "/about-jingcheng/static/css/main.2bd7f39d.chunk.css"
+  },
+  {
+    "revision": "1bbcd8e23da4b06c8599",
+    "url": "/about-jingcheng/static/js/2.2fbdcf0a.chunk.js"
+  },
+  {
+    "revision": "0ac79d53dda6ffa0c6325c90109848cb",
+    "url": "/about-jingcheng/static/js/2.2fbdcf0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "664e8e5aba02faea5da2",
+    "url": "/about-jingcheng/static/js/main.abb4df07.chunk.js"
+  },
+  {
+    "revision": "ea9416b2e064afbcbb62",
+    "url": "/about-jingcheng/static/js/runtime-main.976a6697.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/about-jingcheng/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/about-jingcheng/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/about-jingcheng/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/about-jingcheng/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/about-jingcheng/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/about-jingcheng/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/about-jingcheng/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/about-jingcheng/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/about-jingcheng/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/about-jingcheng/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/about-jingcheng/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/about-jingcheng/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/about-jingcheng/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/about-jingcheng/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/about-jingcheng/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/about-jingcheng/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
