48484b85a372e61507419594e35b612c0ad94b85
jesse7zhangjc <jesse7zhangjc@gmail.com>
2020-07-28 20:34:15 -0700

    Configure prettier

--- src/components/mainContent/resumePage/ResumePage.tsx
+++ src/components/mainContent/resumePage/ResumePage.tsx
@@ -2,15 +2,19 @@ import React, { useEffect } from 'react';
 import { Container, Header, Divider, Transition } from 'semantic-ui-react';
 
 import messages from 'messages';
-import Experience from 'components/mainContent/resumePage/Experience';
-import Education from 'components/mainContent/resumePage/Education';
-
-import 'components/mainContent/resumePage/resumePage.css'
 import { useTransitionEffect, animationDuration } from 'utils/transitionUtil';
 import { HashLink } from 'react-router-hash-link';
 import { IMainContentProps } from 'components/mainContent/MainContent';
+import Experience from 'components/mainContent/resumePage/Experience';
+import Education from 'components/mainContent/resumePage/Education';
 
-const { sideNavigationBar: sbm, resumePage: rm, content: { experiences, educations, contacts } } = messages;
+import 'components/mainContent/resumePage/resumePage.css';
+
+const {
+  sideNavigationBar: sbm,
+  resumePage: rm,
+  content: { experiences, educations, contacts },
+} = messages;
 const resumeSubTitles = [
   rm.experience,
   rm.education,
@@ -21,8 +25,15 @@ const resumeSubTitles = [
 const ResumeSubTitleLinks = () => (
   <div className="subtitles">
     {resumeSubTitles.map(subTitle => (
-      <Header className="resume-subtitle-link" size="small" key={subTitle} as="span">
-        <HashLink smooth to={`/resume#${subTitle.toLowerCase()}-section`}>{subTitle}</HashLink>
+      <Header
+        className="resume-subtitle-link"
+        size="small"
+        key={subTitle}
+        as="span"
+      >
+        <HashLink smooth to={`/resume#${subTitle.toLowerCase()}-section`}>
+          {subTitle}
+        </HashLink>
       </Header>
     ))}
   </div>
@@ -31,11 +42,13 @@ const ResumeSubTitleLinks = () => (
 const ExperienceSection = () => {
   return (
     <div id="experiment-section">
-      <Header className="resume-subtitle" size="large">{rm.experience}</Header>
+      <Header className="resume-subtitle" size="large">
+        {rm.experience}
+      </Header>
       {experiences.map((exp, idx) => (
         <div key={idx}>
           <Experience experience={exp} />
-          {idx !== experiences.length && <Divider hidden section/>}
+          {idx !== experiences.length && <Divider hidden section />}
         </div>
       ))}
     </div>
@@ -45,11 +58,13 @@ const ExperienceSection = () => {
 const EducationSection = () => {
   return (
     <div id="education-section">
-      <Header className="resume-subtitle" size="large">{rm.education}</Header>
+      <Header className="resume-subtitle" size="large">
+        {rm.education}
+      </Header>
       {educations.map((edu, idx) => (
         <div key={idx}>
           <Education education={edu} />
-          {idx !== experiences.length && <Divider hidden section/>}
+          {idx !== experiences.length && <Divider hidden section />}
         </div>
       ))}
     </div>
@@ -59,21 +74,29 @@ const EducationSection = () => {
 type IResumePageProps = IMainContentProps;
 
 const ResumePage = (props: IResumePageProps) => {
-  const showContent = useTransitionEffect(props.sideBarReady ? 0 :animationDuration);
+  const showContent = useTransitionEffect(
+    props.sideBarReady ? 0 : animationDuration
+  );
   useEffect(() => {
     document.title = `${sbm.resume} | ${contacts.fullName}`;
-  }, [])
+  }, []);
   return (
-    <Transition animation="fade right" duration={animationDuration} visible={showContent}>
+    <Transition
+      animation="fade right"
+      duration={animationDuration}
+      visible={showContent}
+    >
       <Container className="resume-page">
         <Divider section hidden />
-        <Header className="resume-title" size="huge">{rm.resume}</Header>
+        <Header className="resume-title" size="huge">
+          {rm.resume}
+        </Header>
         <Divider hidden />
         <ResumeSubTitleLinks />
         <Divider />
-        <Divider hidden section/>
+        <Divider hidden section />
         <ExperienceSection />
-        <Divider hidden section/>
+        <Divider hidden section />
         <EducationSection />
       </Container>
     </Transition>
